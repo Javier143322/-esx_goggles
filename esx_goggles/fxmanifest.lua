7cd781e1bd4e65a406f70bddcfa348e61693c92c
and_res_4 <huhdnuhdl@gmail.com>
2025-10-22 16:23:26 -0500

    Actualizar fxmanifest.lua

--- esx_goggles/fxmanifest.lua
+++ esx_goggles/fxmanifest.lua
@@ -1,25 +1,21 @@
-
 fx_version 'cerulean'
 games { 'gta5' }
 
--- Metadata del Recurso
 name 'ESX Goggles (Night/Thermal Vision)'
 author 'IA Assistant'
 description 'Sistema de Gafas con Visión Nocturna y Térmica, restringido por Facciones (Job).'
 
--- Configuración de ESX
 shared_scripts {
+    '@es_extended/imports.lua',
     '@es_extended/config.lua',
-    'config.lua' -- Debe cargarse primero para que el cliente/servidor lea las configuraciones.
+    'config.lua'
 }
 
--- Código del Lado del Cliente
 client_scripts {
     '@es_extended/client/main.lua',
     'client/client.lua'
 }
 
--- Código del Lado del Servidor
 server_scripts {
     '@es_extended/server/main.lua',
     'server/server.lua'
